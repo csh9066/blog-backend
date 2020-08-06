a2cd817c840190e13c3c08520830c20fcf8653c9
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-06 20:22:58 +0900

    Add app.js

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const apiRouter = require('./api');
+const db = require('./models');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log('db 연결 성공');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+app.use('/api', apiRouter);
+
+app.listen(4000, () => {
+  console.log('linstening to port 4000');
+});
