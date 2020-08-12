26939aa7d768943c89899776eb295b84dd1edbdf
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-12 14:50:29 +0900

    add app.js middlewares

--- src/app.js
+++ src/app.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 
 const apiRouter = require('./api');
 const db = require('./models');
+const passportConfig = require('./passport');
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+    },
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passportConfig();
 
 db.sequelize
-  .sync()
+  .sync({ alter: true })
   .then(() => {
     console.log('db 연결 성공');
   })
   .catch((e) => {
     console.error(e);
   });
-
-app.use(logger('dev'));
 
 app.use('/api', apiRouter);
 
